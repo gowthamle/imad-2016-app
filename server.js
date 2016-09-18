059de6f6ec23a72fa7dc92a0b73bca1c8e480494
gowthamle <legowtham14@gmail.com>
2016-09-18 10:27:05 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ app.get('/', function (req, res) {
 });
 
 app.get('/article-one',function(req,res) {
-    res.sendfile("article one is requested and will display here");
+    res.send("article one is requested and will display here");
 });
 
 app.get('/article-two',function(req,res) {
